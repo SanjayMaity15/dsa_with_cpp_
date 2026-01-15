17433e2d5261fd8fa59d369a4970b79ff23030e2
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2026-01-15 15:00:52 +0530

    first commit

---
+++ basics_cpp/basic.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // cout << "My name is Sanjay Maity"<<endl;
+    // cout << "I am final year student";
+
+    // cout << "I will master c++ and DSA";
+
+    // int a = 10, b = 20, c = 30;
+    // cout << "The value of a , b and c are " << a << " " << b <<" "<< c;
+
+    
+
+    return 0;
+}
