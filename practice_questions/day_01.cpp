f9aa95dc057678b2c01c96af671afeb19212e0f9
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2026-01-16 10:39:07 +0530

    Binary to decimal problem added

--- practice_questions/day_01.cpp
+++ practice_questions/day_01.cpp
@@ -34,20 +34,37 @@ int main()
 
     // LCM
 
-    int n1, n2;
-    cout << "Enter 2 numbers : ";
-    cin >> n1 >> n2;
+    // int n1, n2;
+    // cout << "Enter 2 numbers : ";
+    // cin >> n1 >> n2;
 
-    int maximum = n1 > n2 ? n1 : n2;
-    int lcm;
-    for (int i = maximum; i <= n1 * n2; i++){
-        if(i % n1 == 0 && i % n2 == 0){
-            lcm = i;
-            break;
-        }
+    // int maximum = n1 > n2 ? n1 : n2;
+    // int lcm;
+    // for (int i = maximum; i <= n1 * n2; i++){
+    //     if(i % n1 == 0 && i % n2 == 0){
+    //         lcm = i;
+    //         break;
+    //     }
+    // }
+
+    // cout << lcm;
+
+    // Binary to decimal
+
+    int binaryNumber = 100011001;
+    int result = 0, mul = 1;
+
+    while (binaryNumber != 0)
+    {
+        int digit = binaryNumber % 10;
+        result = (digit * mul) + result;
+        binaryNumber /= 10;
+        mul *= 2;
     }
 
-    cout << lcm;
+    cout << result;
 
+
+    
     return 0;
 }
