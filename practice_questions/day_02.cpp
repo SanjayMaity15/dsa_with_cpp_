6edce75cc791cd9ecb346ac4b33eea1932052053
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2026-01-17 10:37:35 +0530

    basic array 5 question complete

---
+++ practice_questions/day_02.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+int main()
+{
+
+    // find the largest element in the array
+
+    // int arr[8] = {34, 567, 35232, 678, 39, 95, 37, 85};
+
+    // int size = sizeof(arr) / sizeof(arr[0]);
+
+    // int maximum = INT_MIN;
+
+    // for (int i = 0; i < size; i++){
+    //     if(arr[i] > maximum){
+    //         maximum = arr[i];
+    //     }
+    // }
+
+    // cout << "The maximum number is : " << maximum;
+
+    // Find the smallest element in the array
+
+    // int arr[8] = {34, 567, 35232, 678, 9, 95, 37, 85};
+
+    // int size = sizeof(arr) / sizeof(arr[0]);
+
+    // int minimum = INT_MAX;
+
+    // for (int i = 0; i < size; i++){
+    //     if(arr[i] < minimum){
+    //         minimum = arr[i];
+    //     }
+    // }
+
+    // cout << "The minimum element is : " << minimum;
+
+    // // Find the smallest and largest element in the array
+
+    // int arr[8] = {34, 567, 35232, 678, 9, 95, 37, 85};
+
+    // int size = sizeof(arr) / sizeof(arr[0]);
+
+    // int minimum = INT_MAX;
+    // int maximum = INT_MIN;
+
+    // for (int i = 0; i < size; i++){
+    //     if(arr[i] < minimum){
+    //         minimum = arr[i];
+    //     }
+
+    //     if(arr[i] > maximum){
+    //         maximum = arr[i];
+    //     }
+    // }
+
+    // cout << "The minimum element is : " << minimum<<endl;
+    // cout << "The maximum element is : " << maximum<<endl;
+
+    // Find 2nd largest element
+
+    // int arr[8] = {34, 567, 35232, 678, 39, 95, 37, 85};
+
+    // int size = sizeof(arr) / sizeof(arr[0]);
+
+    // int maximum = INT_MIN;
+
+    // int secondMaximum = INT_MIN;
+
+    // for (int i = 0; i < size; i++)
+    // {
+    //     if (arr[i] > maximum)
+    //     {
+    //         secondMaximum = maximum;
+    //         maximum = arr[i];
+    //     }
+    //     else if (arr[i] > secondMaximum && arr[i] < maximum)
+    //     {
+    //         secondMaximum = arr[i];
+    //     }
+    // }
+
+    // cout << secondMaximum;
+
+    // Find second smallest element
+
+    // int arr[8] = {34, 567, 35232, 678, 39, 95, 37, 85};
+
+    // int size = sizeof(arr) / sizeof(arr[0]);
+
+    // int minimum = INT_MAX;
+    // int secondMinimum = INT_MAX;
+
+    // for (int i = 0; i < size; i++){
+    //     if(arr[i] < minimum){
+    //         secondMinimum = minimum;
+    //         minimum = arr[i];
+    //     }
+    //     else if (arr[i] < secondMinimum &&  arr[i] > minimum)
+    //     {
+    //         secondMinimum = arr[i];
+    //     }
+    // }
+
+    // cout << minimum << "  " << secondMinimum;
+
+    return 0;
+}
