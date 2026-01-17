c72c83d510fbe7fa50388b1cd5badb67a709f369
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2026-01-17 10:05:54 +0530

    basic question complete

--- practice_questions/day_01.cpp
+++ practice_questions/day_01.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 using namespace std;
 
+int fact(int number)
+{
+    int fact = 1;
+
+    for (int i = 1; i <= number; i++){
+        fact = fact * i;
+    }
+
+    return fact;
+}
+
+bool isPrime(int number){
+    if(number < 2)
+        return false;
+
+    for (int i = 2; i < number; i++){
+        if(number % i == 0)
+            return false;
+    }
+
+    return true;
+}
+
 int main()
 {
 
@@ -101,18 +124,189 @@ int main()
 
     // decimal to octal
 
-    int decimalNumber = 2134;
-    int result = 0, mul = 1;
+    // int decimalNumber = 2134;
+    // int result = 0, mul = 1;
 
-    while (decimalNumber != 0)
-    {
-        int digit = decimalNumber % 8;
-        result = (digit * mul) + result;
-        decimalNumber /= 8;
-        mul *= 10;
+    // while (decimalNumber != 0)
+    // {
+    //     int digit = decimalNumber % 8;
+    //     result = (digit * mul) + result;
+    //     decimalNumber /= 8;
+    //     mul *= 10;
+    // }
+
+    // cout << result;
+
+    // Quadrant in which coordinate lies
+
+    // int x, y;
+    // cout << "Enter value for x and y : ";
+    // cin >> x >> y;
+
+    // if(x > 0 && y > 0){
+    //     cout << "Quadrant 1";
+    // }else if(x < 0 && y > 0){
+    //     cout << "Quadrant 2";
+    // }else if(x < 0 && y < 0){
+    //     cout << "Quadrant 3";
+    // }else if(x > 0 && y < 0){
+    //     cout << "Quadrant 4";
+    // }else if (x == 0 && y != 0){
+    //     cout << "Y Axis";
+    // }else if(x != 0 && y == 0){
+    //     cout << "X axis";
+    // }else{
+    //     cout << "origin";
+    // }
+
+
+    // permutation
+
+    // formula is = n! / (n - r)!
+
+
+
+    // int n, r;
+
+    // cout << "Enter value for n and r : ";
+    // cin >> n >> r;
+
+    // int result = fact(n) / fact(n - r);
+    // cout << result;
+
+    // Maximum number of handshake
+
+    // formula = (n * (n - 1) ) / 2
+
+    // int n;
+    // cout << "Enter number : ";
+    // cin >> n;
+
+    // cout << (n * (n - 1)) / 2;
+
+
+
+    // replace all the zeros with 1
+
+    // int n = 12090;
+
+    // int result = 0, place = 1;
+
+    // while (n != 0)
+    // {
+    //     int digit = n % 10;
+    //     if(digit == 0){
+    //         digit = 1;
+    //     }
+    //     result = (digit * place) + result;
+    //     n /= 10;
+    //     place *= 10;
+    // }
+
+    // cout << result;
+
+
+    // Whether a number can be express as a sum of two prime    
+    // int n = 10;
+    // bool flag = false;
+
+    // for (int i = 1; i <= (n / 2); i++){
+    //     if(isPrime(i) && isPrime(n - i)){
+    //         cout << i << endl;
+    //         cout << n - i << endl;
+
+    //         flag = true;
+    //     }
+    // }
+
+
+    // if(flag){
+    //     cout << "true";
+    // }else{
+    //     cout << "false";
+    // }
+
+
+    // Area of a circle
+
+    // float r = 5;
+    // float pi = 3.14;
+
+    // cout << "Area of a circle is " << pi * (r * r);
+
+    // Prime number between 1 to 100
+
+    // for (int i = 1; i <= 100; i++){
+    //     if(isPrime(i)){
+    //         cout << i <<" ";
+    //     }
+    // }
+
+
+    // calculate the number of digits of a number
+
+    // int n = 325332399;
+
+    // int digitCount = 0;
+
+    // while (n != 0)
+    // {
+    //     digitCount++;
+    //     n /= 10;
+    // }
+
+    // cout << digitCount;
+
+    // int month = 7, year = 2015;
+
+    // if(month == 2){
+    //     cout << "28 days";
+    // }else if (month == 2 && (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))){
+    //     cout << "29 days";
+    // }
+    // else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+    // {
+    //     cout << "31 days";
+    // }
+    // else if (month == 4 || month == 6 || month == 9 || month == 11){
+    //     cout << "30 days";
+    // }else{
+    //     cout << "Invalid";
+    // }
+
+    // int n = 373272588;
+    // int find = 2;
+    // int count = 0;
+
+    // while (n != 0)
+    // {
+    //     int digit = n % 10;
+    //     if(digit == find)
+    //         count++;
+
+    //     n /= 10;
+    // }
+
+    // cout << count;
+
+    int n = 10, x = 3;
+
+    int finalCount = 0;
+    for (int i = 1; i <= n; i++){
+
+        int divisor = 0;
+        for (int j = 1; j <= i; j++){
+            if(i % j == 0){
+                divisor++;
+            }
+        }
+
+        if(divisor == x){
+            finalCount++;
+        }
     }
 
-    cout << result;
+    cout << finalCount;
 
     return 0;
 }
